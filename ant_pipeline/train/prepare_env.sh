56766aba80f4e93b4c7a7b2c2b64509180f2cdc4
hexiao.xq <hexiao.xq@antgroup.com>
2025-08-15 17:51:33 +0800

    [Ant-Pipeline]:优化环境脚本

--- ant_pipeline/train/prepare_env.sh
+++ ant_pipeline/train/prepare_env.sh
@@ -2,12 +2,14 @@ set -ex
 
 script_dir=$(cd "$(dirname "$0")" && pwd)
 
+pip install --upgrade pip setuptools wheel
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --use-pep517 ninja
+
 pushd $script_dir/../../
-pip install -r requirements.txt
+# pip install -r requirements.txt # 默认环境已装好 
 pip install -e .
 popd
 
+# MAX_JOBS=200 pip install flash-attn --no-build-isolation --no-cache-dir --force-reinstall # 默认环境已装好 
 pip install jinja2==3.1.2
-
-# if use 
-# pip install sglang[all] vllm
+# pip install deepspeed
